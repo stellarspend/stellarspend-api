c5ec89165f8911d8c3ec914e0550937e435597dc
Eniola3321 <ekanadeadeniyiabdulhafis@gmail.com>
2026-02-28 11:45:20 +0100

    fix: Standardize API error responses

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3001);
 }
